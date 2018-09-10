d62cbdb10434994c3d816c56ef9210e73884210c
Jesper Larsson <jesper.a.larsson@outlook.com>
2018-09-10 13:00:15 +0200

    Prevent multiple threads to update a HostEndPoints cache concurrently.

--- sdk/src/Core/Internal/Utils/HostEndPoint.cs
+++ sdk/src/Core/Internal/Utils/HostEndPoint.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using Amazon.Runtime.Internal.Util;
 
 namespace Amazon.XRay.Recorder.Core.Internal.Utils
@@ -12,6 +13,7 @@ namespace Amazon.XRay.Recorder.Core.Internal.Utils
 		private IPEndPoint _ipCache = null;
 		private DateTime? _timestampOfLastIPCacheUpdate = null;
 		private static readonly Logger _logger = Logger.GetLogger(typeof(HostEndPoint));
+		private ReaderWriterLockSlim cacheLock = new ReaderWriterLockSlim();
 
 		public HostEndPoint(string host, int port)
 		{
@@ -25,59 +27,100 @@ namespace Amazon.XRay.Recorder.Core.Internal.Utils
 
 		private bool isIPCacheValid()
 		{
+			cacheLock.EnterReadLock();
+
+			bool res;
+			
 			if (_ipCache == null)
 			{
-				return false;
+				res = false;
 			}
 
 			if (_timestampOfLastIPCacheUpdate is DateTime lastTimestamp)
 			{
-				return DateTime.Now.Subtract(lastTimestamp).TotalSeconds < CACHE_TTL;
+				res = DateTime.Now.Subtract(lastTimestamp).TotalSeconds < CACHE_TTL;
 			}
 			else
 			{
-				return false;
+				res = false;
+			}
+			
+			cacheLock.ExitReadLock();
+			return res;
+		}
+
+		private void updateCache()
+		{
+			//If we fail with timeout = 0 another thread is already updating the cache and we don't need to it as well
+			bool entered = cacheLock.TryEnterReadLock(0);
+
+			if (!entered)
+			{
+				return;
+			}
+			
+			try
+			{
+				var ipEntries = Dns.GetHostAddresses(Host);
+				var newIP = ipEntries.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+				if (newIP != null)
+				{
+					_timestampOfLastIPCacheUpdate = DateTime.Now;
+					_logger.InfoFormat("IP cache invalid: updated ip cache for {0} to {1}.", Host, newIP);
+					_ipCache = new IPEndPoint(newIP, Port);
+				}
+				else
+				{
+					_logger.InfoFormat(
+						"IP cache invalid: DNS responded with zero IP addresses for {1}. Falling back to cached IP, e.g. {0}.",
+						_ipCache, Host);
+				}
+			}
+			//Error catching for DNS resolve
+			catch (ArgumentNullException)
+			{
+				_logger.InfoFormat(
+					"IP cache invalid: failed to resolve DNS due to host being null. Falling back to cached IP, e.g. {0}.",
+					_ipCache);
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				_logger.InfoFormat(
+					"IP cache invalid: failed to resolve DNS due to host being longer than 255 characters. ({0})",
+					Host);
+			}
+			catch (SocketException)
+			{
+				_logger.InfoFormat("IP cache invalid: failed to resolve DNS. ({0})", Host);
+			}
+			catch (ArgumentException)
+			{
+				_logger.InfoFormat("IP cache invalid: failed to update cache due to {0} not being a vaild IP.", Host);
+			}
+			finally
+			{
+				cacheLock.ExitWriteLock();
 			}
 		}
 
+		/// <summary>
+		/// Returns a cached ip resolved from the host.
+		///
+		/// If the cache is invalid this method will update it.
+		///
+		/// The cache returned may be dirty. This may occur since the cache has a TTL
+		/// but the host-ip mapping may change in that time. It may also occur when
+		/// this thread tries to update the cache but another thread is already doing so.
+		/// </summary>
+		/// <returns></returns>
 		public IPEndPoint GetIPEndPoint()
 		{
+			
 			if (!isIPCacheValid())
 			{
-				try
-				{
- 					var ipEntries = Dns.GetHostAddresses(Host);
-					var newIP = ipEntries.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
-					if (newIP != null)
-					{
-						_timestampOfLastIPCacheUpdate = DateTime.Now;
-						_logger.InfoFormat("IP cache invalid: updated ip cache for {0} to {1}.", Host, newIP);
-						_ipCache = new IPEndPoint(newIP, Port);
-					}
-					else
-					{
-						_logger.InfoFormat("IP cache invalid: DNS responded with zero IP addresses for {1}. Falling back to cached IP, e.g. {0}.", _ipCache, Host);
-					}
-				}
-				//Error catching for DNS resolve
-				catch(ArgumentNullException)
-				{
-					_logger.InfoFormat("IP cache invalid: failed to resolve DNS due to host being null. Falling back to cached IP, e.g. {0}.", _ipCache);
-				}
-				catch(ArgumentOutOfRangeException)
-				{
-					_logger.InfoFormat("IP cache invalid: failed to resolve DNS due to host being longer than 255 characters. ({0})", Host);
-				}
-				catch(SocketException)
-				{
-					_logger.InfoFormat("IP cache invalid: failed to resolve DNS. ({0})", Host);
-				}
-				catch(ArgumentException)
-				{
-					_logger.InfoFormat("IP cache invalid: failed to update cache due to {0} not being a vaild IP.", Host);
-				}
+				updateCache();
 			}
-
+			
 			return _ipCache;
 		}
 	}
