f8ff6c25161742a71f3d132d00169667f48919b4
Prashant <srprash@amazon.com>
2020-03-05 21:05:54 -0800

    Implementing support for tracing EntityFramework Core

---
+++ sdk/src/Handlers/EntityFramework/EFInterceptor.netstandard.cs
@@ -0,0 +1,222 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Amazon.XRay.Recorder.Core;
+using Amazon.XRay.Recorder.Core.Internal.Entities;
+using Amazon.XRay.Recorder.Core.Exceptions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Amazon.XRay.Recorder.Handlers.EntityFramework
+{
+    public class EFInterceptor : Microsoft.EntityFrameworkCore.Diagnostics.DbCommandInterceptor
+    {
+        private const string DataBaseTypeString = "sqlserver";
+        private readonly AWSXRayRecorder _recorder;
+        private readonly bool? _collectSqlQueriesOverride;
+
+        public EFInterceptor() : this(AWSXRayRecorder.Instance)
+        {
+
+        }
+
+        public EFInterceptor(bool? collectSqlQueries = null) : this(AWSXRayRecorder.Instance, collectSqlQueries)
+        {
+
+        }
+
+        public EFInterceptor(AWSXRayRecorder recorder, bool? collectSqlQueries = null) : base()
+        {
+            _recorder = recorder;
+            _collectSqlQueriesOverride = collectSqlQueries;
+        }
+
+        public override InterceptionResult<DbDataReader> ReaderExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result)
+        {
+            ProcessBeginCommand(command);
+            return base.ReaderExecuting(command, eventData, result);
+        }
+
+        public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
+        {
+            ProcessEndCommand(command);
+            return base.ReaderExecuted(command, eventData, result);
+        }
+
+        public override Task<InterceptionResult<DbDataReader>> ReaderExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result, CancellationToken cancellationToken = default)
+        {
+            ProcessBeginCommand(command);
+            return base.ReaderExecutingAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override Task<DbDataReader> ReaderExecutedAsync(DbCommand command, CommandExecutedEventData eventData, DbDataReader result, CancellationToken cancellationToken = default)
+        {
+            ProcessEndCommand(command);
+            return base.ReaderExecutedAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override void CommandFailed(DbCommand command, CommandErrorEventData eventData)
+        {
+            ProcessCommandError(command, eventData);
+            base.CommandFailed(command, eventData);
+        }
+
+        public override Task CommandFailedAsync(DbCommand command, CommandErrorEventData eventData, CancellationToken cancellationToken = default)
+        {
+            ProcessCommandError(command, eventData);
+            return base.CommandFailedAsync(command, eventData, cancellationToken);
+        }
+
+        public override InterceptionResult<int> NonQueryExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<int> result)
+        {
+            ProcessBeginCommand(command);
+            return base.NonQueryExecuting(command, eventData, result);
+        }
+
+        public override Task<InterceptionResult<int>> NonQueryExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            ProcessBeginCommand(command);
+            return base.NonQueryExecutingAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override int NonQueryExecuted(DbCommand command, CommandExecutedEventData eventData, int result)
+        {
+            ProcessEndCommand(command);
+            return base.NonQueryExecuted(command, eventData, result);
+        }
+
+        public override Task<int> NonQueryExecutedAsync(DbCommand command, CommandExecutedEventData eventData, int result, CancellationToken cancellationToken = default)
+        {
+            ProcessEndCommand(command);
+            return base.NonQueryExecutedAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override InterceptionResult<object> ScalarExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<object> result)
+        {
+            ProcessBeginCommand(command);
+            return base.ScalarExecuting(command, eventData, result);
+        }
+
+        public override Task<InterceptionResult<object>> ScalarExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<object> result, CancellationToken cancellationToken = default)
+        {
+            ProcessBeginCommand(command);
+            return base.ScalarExecutingAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override object ScalarExecuted(DbCommand command, CommandExecutedEventData eventData, object result)
+        {
+            ProcessEndCommand(command);
+            return base.ScalarExecuted(command, eventData, result);
+        }
+
+        public override Task<object> ScalarExecutedAsync(DbCommand command, CommandExecutedEventData eventData, object result, CancellationToken cancellationToken = default)
+        {
+            ProcessEndCommand(command);
+            return base.ScalarExecutedAsync(command, eventData, result, cancellationToken);
+        }
+
+        private void ProcessBeginCommand(DbCommand command)
+        {
+            Entity entity = null;
+            try
+            {
+                entity = _recorder.GetEntity();
+            }
+            catch (EntityNotAvailableException e)
+            {
+                _recorder.TraceContext.HandleEntityMissing(_recorder, e, "Cannot get entity while processing start of Entity Framework command.");
+            }
+
+            //_recorder.BeginSubsegment(SqlUtil.BuildSubsegmentName(command)); //commented because its failing with invalid name due to \\
+            _recorder.BeginSubsegment("EF_sub");
+            _recorder.SetNamespace("remote");
+            CollectSqlInformation(command);
+        }
+
+        private void ProcessEndCommand(DbCommand command)
+        {
+            Entity entity = null;
+            try
+            {
+                entity = _recorder.GetEntity();
+            }
+            catch (EntityNotAvailableException e)
+            {
+                _recorder.TraceContext.HandleEntityMissing(_recorder, e, "Cannot get entity while processing end of Entity Framework command.");
+                return;
+            }
+
+            //CollectSqlInformation(command); //moving to processbegincommand so that errors would also have sql info.
+            _recorder.EndSubsegment();
+        }
+
+        private void ProcessCommandError(DbCommand command, CommandErrorEventData eventData)
+        {
+            Entity subsegment;
+            try
+            {
+                subsegment = _recorder.GetEntity();
+            }
+            catch (EntityNotAvailableException e)
+            {
+                _recorder.TraceContext.HandleEntityMissing(_recorder, e, "Cannot get entity while processing failure of Entity Framework command.");
+                return;
+            }
+
+            subsegment.AddException(eventData.Exception);
+
+            _recorder.EndSubsegment();
+        }
+
+        /// <summary>
+        /// Records the SQL information on the current subsegment,
+        /// </summary>
+        protected virtual void CollectSqlInformation(DbCommand command)
+        {
+            _recorder.AddSqlInformation("database_type", DataBaseTypeString);
+
+            _recorder.AddSqlInformation("database_version", command.Connection.ServerVersion);
+
+            //SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder(command.Connection.ConnectionString);
+
+            DbConnectionStringBuilder connectionStringBuilder = new DbConnectionStringBuilder();
+            connectionStringBuilder.ConnectionString = command.Connection.ConnectionString;
+
+            // Remove sensitive information from connection string
+            connectionStringBuilder.Remove("Password");
+
+            // Do a pre-check for UserID since in the case of TrustedConnection, a UserID may not be available.
+            var user_id = GetConnectionValue(connectionStringBuilder, "User ID");
+            if (user_id != null)
+            {
+                _recorder.AddSqlInformation("user", user_id.ToString());
+            }
+            
+            _recorder.AddSqlInformation("connection_string", connectionStringBuilder.ToString());
+
+            if (ShouldCollectSqlText())
+            {
+                _recorder.AddSqlInformation("sanitized_query", command.CommandText);
+            }
+        }
+
+        private object GetConnectionValue(DbConnectionStringBuilder builder, string key)
+        {
+            object value = null;
+            if (key == null)
+            {
+                return null;
+            }
+            builder.TryGetValue(key, out value);
+            return value;
+        }
+
+#if !NET45
+        private bool ShouldCollectSqlText()
+            => _collectSqlQueriesOverride ?? _recorder.XRayOptions.CollectSqlQueries;
+#else
+        private bool ShouldCollectSqlText()
+            => _collectSqlQueriesOverride ?? AppSettings.CollectSqlQueries;
+#endif
+    }
+}
---
+++ sdk/test/UnitTests/EfCoreTests.cs
@@ -0,0 +1,116 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="EFCoreTests.cs" company="Amazon.com">
+//      Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+//      Licensed under the Apache License, Version 2.0 (the "License").
+//      You may not use this file except in compliance with the License.
+//      A copy of the License is located at
+//
+//      http://aws.amazon.com/apache2.0
+//
+//      or in the "license" file accompanying this file. This file is distributed
+//      on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//      express or implied. See the License for the specific language governing
+//      permissions and limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.XRay.Recorder.UnitTests.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Amazon.XRay.Recorder.Handlers.EntityFramework;
+using Amazon.XRay.Recorder.Core;
+using Amazon.XRay.Recorder.Core.Internal.Utils;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Amazon.XRay.Recorder.Core.Internal.Entities;
+
+namespace Amazon.XRay.Recorder.UnitTests
+{
+    [TestClass]
+    public class EfCoreTests : TestBase
+    {
+        private const String _connectionString = "datasource=:memory:";
+        private const String _queryString = "SELECT \"u\".\"UserId\"\r\nFROM \"Users\" AS \"u\"\r\nWHERE \"u\".\"UserId\" = 1";
+        private const String _dbVersion = "3.28.0";
+        private SqliteConnection connection = null;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // In-memory database only exists while the connection is open
+            connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+        }
+
+        [TestCleanup]
+        public new void TestCleanup()
+        {
+            connection.Close();
+            base.TestCleanup();
+        }
+
+        [TestMethod]
+        public void Test_EFCore_successful_query()
+        {
+            // Arrange
+#if !NET45
+            var recorder = new AWSXRayRecorder
+            {
+                XRayOptions = new XRayOptions { CollectSqlQueries = true }
+            };
+#else
+            var recorder = new AWSXRayRecorder();
+#endif
+            recorder.BeginSegment("TestSegment");
+            var context = GetTestEFContext();
+            // Act
+            var users = context.Users.Where(u => u.UserId == 1).ToList();
+
+            // Assert
+            var segment = recorder.TraceContext.GetEntity();
+            Assert.AreEqual(3, segment.Subsegments.Count); //2 subsegments from validating the db and 1 from the actual query
+            var query_subsegment = segment.Subsegments[2];
+            AssertQueryCollected(query_subsegment);
+
+            recorder.EndSegment();
+        }
+
+        private void AssertQueryCollected(Subsegment subsegment)
+        {
+            AssertExpectedSqlInformation(subsegment);
+            Assert.AreEqual(_queryString, subsegment.Sql["sanitized_query"]);
+        }
+
+        private void AssertQueryNotCollected(Subsegment subsegment)
+        {
+            AssertExpectedSqlInformation(subsegment);
+            Assert.IsFalse(subsegment.Sql.ContainsKey("sanitized_query"));
+        }
+
+        private void AssertExpectedSqlInformation(Subsegment subsegment)
+        {
+            Assert.IsNotNull(subsegment);
+            Assert.IsNotNull(subsegment.Sql);
+            Assert.AreEqual(_connectionString, subsegment.Sql["connection_string"]);
+            Assert.AreEqual(connection.ServerVersion, subsegment.Sql["database_version"]);
+        }
+
+
+
+        private TestEFContext GetTestEFContext()
+        {
+            var options = new DbContextOptionsBuilder<TestEFContext>()
+                .UseSqlite(connection)
+                .AddInterceptors(new EFInterceptor())
+                .Options;
+            var context = new TestEFContext(options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
