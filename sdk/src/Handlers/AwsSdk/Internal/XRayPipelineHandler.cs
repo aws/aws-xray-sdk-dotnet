412185fa732d41f1cfb727c8041a77babc4074b4
Lu Peng <61207760+lupengamzn@users.noreply.github.com>
2020-12-29 14:21:52 -0800

    Enable tracing S3 id pairs and fix SNS naming issue. (#168)
    * Enable tracing S3 id pairs and fix SNS naming issue.
    
    * Changed AWSSDK.Core version
    
    * Changed parameter names and updated comments.
    
    * Set parameters as default

--- sdk/src/Handlers/AwsSdk/Internal/XRayPipelineHandler.cs
+++ sdk/src/Handlers/AwsSdk/Internal/XRayPipelineHandler.cs
@@ -44,9 +44,6 @@ namespace Amazon.XRay.Recorder.Handlers.AwsSdk.Internal
         private const string DefaultAwsWhitelistManifestResourceName = "Amazon.XRay.Recorder.Handlers.AwsSdk.DefaultAWSWhitelist.json";
         private static readonly Logger _logger = Runtime.Internal.Util.Logger.GetLogger(typeof(AWSXRayRecorder));
         private AWSXRayRecorder _recorder;
-        private const String S3RequestIdHeaderKey = "x-amz-request-id";
-        private const String S3ExtendedRequestIdHeaderKey = "x-amz-id-2";
-        private const String ExtendedRquestIdSegmentKey = "id_2";
 
         /// <summary>
         /// Gets AWS service manifest of operation parameter whitelist.
@@ -319,7 +316,8 @@ namespace Amazon.XRay.Recorder.Handlers.AwsSdk.Internal
                 _recorder.TraceContext.HandleEntityMissing(_recorder, e, "Cannot get entity while processing AWS SDK request");
             }
 
-            _recorder.BeginSubsegment(RemoveAmazonPrefixFromServiceName(request.ServiceName));
+            var serviceName = RemoveAmazonPrefixFromServiceName(request.ServiceName);
+            _recorder.BeginSubsegment(AWSXRaySDKUtils.FormatServiceName(serviceName));
             _recorder.SetNamespace("aws");
 
             entity = entity == null ? null : _recorder.GetEntity();
@@ -380,20 +378,27 @@ namespace Amazon.XRay.Recorder.Handlers.AwsSdk.Internal
                 // s3 doesn't follow request header id convention
                 else
                 {
-                    if (requestContext.Request.Headers.TryGetValue(S3RequestIdHeaderKey, out requestId))
+                    if (requestContext.Request.Headers.TryGetValue("x-amz-request-id", out requestId))
                     {
                         subsegment.Aws["request_id"] = requestId;
                     }
 
-                    if (requestContext.Request.Headers.TryGetValue(S3ExtendedRequestIdHeaderKey, out requestId))
+                    if (requestContext.Request.Headers.TryGetValue("x-amz-id-2", out requestId))
                     {
-                        subsegment.Aws[ExtendedRquestIdSegmentKey] = requestId;
+                        subsegment.Aws["id_2"] = requestId;
                     }
                 }
             }
             else
             {
                 subsegment.Aws["request_id"] = responseContext.Response.ResponseMetadata.RequestId;
+
+                // try getting x-amz-id-2 if dealing with s3 request
+                if (responseContext.Response.ResponseMetadata.Metadata.TryGetValue("x-amz-id-2", out string extendedRequestId))
+                {
+                    subsegment.Aws["id_2"] = extendedRequestId;
+                }
+
                 AddResponseSpecificInformation(serviceName, operation, responseContext.Response, subsegment.Aws);
             }
 
@@ -451,6 +456,13 @@ namespace Amazon.XRay.Recorder.Handlers.AwsSdk.Internal
             _recorder.AddHttpInformation("response", responseAttributes);
 
             subsegment.Aws["request_id"] = ex.RequestId;
+
+            // AmazonId2 property in AmazonS3Exception corresponds to the x-amz-id-2 Http header
+            var property = ex.GetType().GetProperty("AmazonId2");
+            if (property != null)
+            {
+                subsegment.Aws["id_2"] = (string)property.GetValue(ex, null);
+            }
         }
 
         private void AddRequestSpecificInformation(string serviceName, string operation, AmazonWebServiceRequest request, IDictionary<string, object> aws)
