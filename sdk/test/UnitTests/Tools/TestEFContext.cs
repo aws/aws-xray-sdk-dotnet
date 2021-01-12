659207cc8bfa811f323937bfb06826d4decc9597
Bolu Peng <bolpeng@amazon.com>
2021-01-11 16:25:04 -0800

    Updated header time

--- sdk/test/UnitTests/Tools/TestEFContext.cs
+++ sdk/test/UnitTests/Tools/TestEFContext.cs
@@ -1,6 +1,6 @@
 ﻿//-----------------------------------------------------------------------------
 // <copyright file="TestEFContext.cs" company="Amazon.com">
-//      Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//      Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 //      Licensed under the Apache License, Version 2.0 (the "License").
 //      You may not use this file except in compliance with the License.
