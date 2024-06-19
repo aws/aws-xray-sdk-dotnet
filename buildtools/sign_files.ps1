d3e429d0f8327906469b4310b7cca0fbddfe6fbd
Prashant Srivastava <srprash@amazon.com>
2024-06-19 16:37:04 -0700

    run aws cli with text output to strip quotes. REVERT LATER

--- buildtools/sign_files.ps1
+++ buildtools/sign_files.ps1
@@ -85,7 +85,7 @@ Begin
 
         $retryCount = 0        
         do {
-            $jobId = aws s3api get-object-tagging --bucket $unsignedS3bucket --key $key --version-id $versionId --query 'TagSet[?Key==`signer-job-id`].Value | [0]'
+            $jobId = aws s3api get-object-tagging --bucket $unsignedS3bucket --key $key --version-id $versionId --query 'TagSet[?Key==`signer-job-id`].Value | [0]' --output text
             $retryCount++
         } while ($jobId -eq "null" -and $retryCount -le $maxRetryCount)
 
